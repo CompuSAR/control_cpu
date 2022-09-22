356e9bd0f0dd084f177d1d67b8e0fd2495d56572
Shachar Shemesh <shachar@shemesh.biz>
2022-09-22 18:41:55 +0300

    Skeleton project

---
+++ ControlCpu.srcs/sources_1/new/top.sv
@@ -0,0 +1,28 @@
+`timescale 1ns / 1ps
+//////////////////////////////////////////////////////////////////////////////////
+// Company: 
+// Engineer: 
+// 
+// Create Date: 09/22/2022 06:24:17 PM
+// Design Name: 
+// Module Name: top
+// Project Name: 
+// Target Devices: 
+// Tool Versions: 
+// Description: 
+// 
+// Dependencies: 
+// 
+// Revision:
+// Revision 0.01 - File Created
+// Additional Comments:
+// 
+//////////////////////////////////////////////////////////////////////////////////
+
+
+module top(
+    input clock,
+    input reset,
+    output running
+    );
+endmodule
