dc5af217df11797c6959425f509c659e55d823ed
Shachar Shemesh <shachar@shemesh.biz>
2023-03-26 16:55:29 +0300

    Memory testing in a loop

--- ControlCpu.srcs/saros/bl1/bl1_start.cpp
+++ ControlCpu.srcs/saros/bl1/bl1_start.cpp
@@ -17,55 +17,77 @@ void bl1_start() {
 
     uart_send("Memory initialized\n");
 
-    for( unsigned int i=0; i<MEMORY_SIZE; ++i ) {
-        if( i%(1024*1024/4)==0 ) {
-            uart_send("Filled ");
-            print_hex(i/(1024*1024/4));
-            uart_send(" MB\n");
+    uint32_t offset = 0;
+
+    uint32_t total_failures = 0;
+    uint32_t cycle = 0;
+    while(true) {
+        uint32_t num_failures = 0;
+
+        for( unsigned int i=0; i<MEMORY_SIZE; ++i ) {
+            if( i%(1024*1024/4)==0 ) {
+                uart_send("Filled ");
+                print_hex(i/(1024*1024/4));
+                uart_send(" MB\n");
+            }
+
+            unsigned int j = i; //(i*RANDOM_WALK_COEF) % MEMORY_SIZE;
+            unsigned int val = (j+offset)*FIBONACCI_COEF;
+            DDR_MEMORY[ j ] = val;
+            unsigned int readback = DDR_MEMORY[ j ];
+            if( val!=readback ) {
+                uart_send("Verify after read failed at ");
+                print_hex(j*4);
+                uart_send(": wrote ");
+                print_hex(val);
+                uart_send(", read back ");
+                print_hex(readback);
+                uart_send(", reread ");
+                print_hex(DDR_MEMORY[ j ]);
+                uart_send("\n");
+
+                num_failures++;
+            }
         }
 
-        unsigned int j = i; //(i*RANDOM_WALK_COEF) % MEMORY_SIZE;
-        unsigned int val = j*FIBONACCI_COEF;
-        DDR_MEMORY[ j ] = val;
-        unsigned int readback = DDR_MEMORY[ j ];
-        if( val!=readback ) {
-            uart_send("Verify after read failed at ");
-            print_hex(j*4);
-            uart_send(": wrote ");
-            print_hex(val);
-            uart_send(", read back ");
-            print_hex(readback);
-            uart_send(", reread ");
-            print_hex(DDR_MEMORY[ j ]);
-            uart_send("\n");
+        uart_send("Filled all memory. Beginning verify.\n");
+
+        unsigned int num = offset*FIBONACCI_COEF;
+        for( unsigned int i=0; i<MEMORY_SIZE; ++i ) {
+            if( i%(1024*1024/4)==0 ) {
+                uart_send("Verified ");
+                print_hex(i/(1024*1024/4));
+                uart_send(" MB\n");
+            }
+
+            unsigned int val=DDR_MEMORY[ i ];
+            if( val != num ) {
+                uart_send("Verification failed: Memory location ");
+                print_hex(i*4);
+                uart_send(" should have been ");
+                print_hex(num);
+                uart_send(". Instead it's ");
+                print_hex(val);
+                uart_send("\n");
+
+                num_failures++;
+            }
+
+            num+=FIBONACCI_COEF;
         }
+
+        total_failures += num_failures;
+
+        uart_send("Verification cycle ");
+        print_hex(++cycle);
+        uart_send(" complete with ");
+        print_hex(num_failures);
+        uart_send(" new failures and ");
+        print_hex(total_failures);
+        uart_send(" total failures\n");
+
+        offset += 0x54d52cb9;
     }
-
-    uart_send("Filled all memory. Beginning verify.\n");
-
-    unsigned int num = 0;
-    for( unsigned int i=0; i<MEMORY_SIZE; ++i ) {
-        if( i%(1024*1024/4)==0 ) {
-            uart_send("Verified ");
-            print_hex(i/(1024*1024/4));
-            uart_send(" MB\n");
-        }
-
-        unsigned int val=DDR_MEMORY[ i ];
-        if( val != num ) {
-            uart_send("Verification failed: Memory location ");
-            print_hex(i*4);
-            uart_send(" should have been ");
-            print_hex(num);
-            uart_send(". Instead it's ");
-            print_hex(val);
-            uart_send("\n");
-        }
-
-        num+=FIBONACCI_COEF;
-    }
-
-    uart_send("Verification complete\n");
 
     halt();
 
